0590ac07777e8cfbccb77d36e2229adb017665eb
Gabriel Zanetti <140424631+GLzanetti@users.noreply.github.com>
2025-01-28 23:10:35 -0300

    Começando os métodos da classe Campo e fazendo testes unitários com JUnit

---
+++ Campo-Minado/teste/br/com/GLzanetti/cm/modelo/CampoTeste.java
@@ -0,0 +1,149 @@
+package br.com.GLzanetti.cm.modelo;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.GLzanetti.cm.excecao.ExplosaoException;
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3,3);
+	}
+	
+	@Test
+	void testeVizinhoEsquerda() {
+		Campo vizinho = new Campo(3,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDireita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoEmcima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoEmbaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDiagonal1() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDiagonal2() {
+		Campo vizinho = new Campo(2, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDiagonal3() {
+		Campo vizinho = new Campo(4, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDiagonal4() {
+		Campo vizinho = new Campo(4, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(5, 6);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		
+		assertFalse(resultado);
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoDuasChamdas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeValorPadraoAtributoMarcado() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoNaoMarcado() {
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrir();
+		});
+	}
+	
+	@Test
+	void testeAbrirComVizinho() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo22 = new Campo(2,2);
+		
+		campo22.adicionarVizinho(campo11);
+		
+		campo.adicionarVizinho(campo22);		
+		campo.abrir();		
+		
+		assertTrue(campo22.isAberto() && campo11.isAberto());
+	}
+	
+}
